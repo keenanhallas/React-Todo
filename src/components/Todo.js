725029415f5cf7e2dd68c1334d7f71121b5ad6b8
Keenan <keenanhallas@gmail.com>
2020-06-09 02:19:11 -0500

    mark complete button now changing to mark incomplete if task is already completed

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -4,7 +4,7 @@ const Todo = ({task, markComplete}) => {
     return (
         <div className="todo" style={task.completed ? {border: "1px solid black", margin: "2%", padding: "2%", textDecoration: "line-through"} : {border: "1px solid black", margin: "2%", padding: "2%"}}>
             <h2>{task.name}</h2>
-            <button onClick={event => {markComplete(event, task.id)}}>Mark complete</button>
+            <button onClick={event => {markComplete(event, task.id)}}>{task.completed ? "Mark incomplete" : "Mark complete"}</button>
         </div>
     );
 }
